463a66e7b055a69b813a1bb92aa5bd9ab2e526a7
Dhrumit13 <prajapatidhrumit@gmail.com>
2024-03-10 17:48:46 +0530

    user in material ui

--- src/app/views/users/users.module.ts
+++ src/app/views/users/users.module.ts
@@ -15,13 +15,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersComponent } from './users.component';
 import { UsersDetailComponent } from './users-detail/users-detail.component';
-import {UsersService} from './services/users.service';
+import { UsersService } from './services/users.service';
 import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
 
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [UsersComponent, UsersDetailComponent],
   imports: [
@@ -41,6 +42,8 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatNativeDateModule,
     MatDatepickerModule,
-  ], providers: [UsersService],
+    MatSelectModule,
+  ],
+  providers: [UsersService],
 })
-export class UsersModule { }
+export class UsersModule {}
